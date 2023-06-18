be2bc0582a9be53171d4de287d5d523f3e486c83
Julus5 <matveev.dulustaan@mail.ru>
2023-06-19 01:59:57 +0700

    Третье задание полностью готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,13 @@
 function compareArrays(arr1, arr2) {
-  
+    if (arr1.length === arr2.length && arr1.every((value, index) => value === arr2[index])) {
+        return true;
+    }
+    else {
+        return false;
+    }
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    let result = users.filter(users => users.gender === gender).map(gender => gender.age);
+    return result.reduce(((acc, amount) => acc + amount / result.length),0);
 }
